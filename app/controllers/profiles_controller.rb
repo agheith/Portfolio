0fb485fbcf6a25f2d111cbb8562e27eb26067140
agheith <amerghth@gmail.com>
2017-04-27 15:13:06 -0700

    Integrated new and create actions for profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,5 +4,27 @@ class ProfilesController < ApplicationController
         @profile_itmes = Profile.all
     end
 
+    def new
+        @portfolio_item = Profile.new
+    end
+
+    def create
+      @portfolio_item = Profile.new(portfolio_params)
+
+      respond_to do |format|
+        if @portfolio_item.save
+          format.html { redirect_to profiles_path, notice: 'Item was successfully created.' }
+        else
+          format.html { render :new }
+        end
+      end
+    end
+
+
+    private
+
+    def portfolio_params
+      params.require(:profile).permit(:title, :subtitle, :body)
+    end
 
 end
